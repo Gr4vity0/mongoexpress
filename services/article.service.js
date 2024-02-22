d41d7f94a6caea561419b30de7830705b532d9f0
Gr4vity0 <44358127+Gr4vity0@users.noreply.github.com>
2024-02-22 13:24:00 +0100

    reglage du problème de slug (duplicate key)

--- services/article.service.js
+++ services/article.service.js
@@ -1,13 +1,29 @@
 const Article = require("../models/article.models");
+const { nanoid } = require("nanoid");
 
 const articleService = {
 
 
      //* Creation de l'article à sauver en DB
     create: async ({title, slug, tag, desc, content}) => {
+        
+        let slugFinal = slug.toLowerCase().trim().replaceAll(/\s/g, '-');
+        
+        //check si le slug existe
+        const articleExists = await Article.findOne({
+            slug: slugFinal
+        })
+
+        // un article trouvé avec le slug -> Modifier le slug
+        if(articleExists){
+            const slugSufix = nanoid(5);
+            slugFinal = slugFinal + '_' + slugSufix;
+
+            //! TODO idéalement, il faut recheck le slug ;)
+        }
         const articleCreated = new Article({
             title,
-            slug: slug.toLowerCase(),
+            slug: slugFinal,
             tag,
             desc: desc || content.slice(0, 100),
             content
